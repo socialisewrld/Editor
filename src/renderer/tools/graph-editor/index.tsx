4d9ed362076071b1ee9768e0d5b7ee6b9e35a00e
Julien Moreau-Mathis <moreau-mathis@intechinfo.fr>
2021-07-17 13:18:27 +0200

    graph editor

--- src/renderer/tools/graph-editor/index.tsx
+++ src/renderer/tools/graph-editor/index.tsx
@@ -2,15 +2,34 @@ import { IStringDictionary } from "../../../shared/types";
 
 import * as React from "react";
 import { Layout, Model, TabNode } from "flexlayout-react";
+import {
+    ButtonGroup, Menu, MenuDivider, MenuItem, Popover, Button, Position, Divider,
+    ContextMenu, Classes,
+} from "@blueprintjs/core";
+
+import { Icon } from "../../editor/gui/icon";
+
+import { undoRedo } from "../../editor/tools/undo-redo";
 
 import { AbstractEditorPlugin, IEditorPluginProps } from "../../editor/tools/plugin";
 
-import layoutConfiguration from "./layout.json";
 import { GraphComponent } from "./graph";
+import layoutConfiguration from "./layout.json";
 
 export const title = "Graph Editor";
 
-export default class GraphEditorPlugin extends AbstractEditorPlugin<{}> {
+export interface IGraphEditorPluginState {
+    /**
+     * Defines wether or not the graph is playing.
+     */
+    playing: boolean;
+    /**
+     * Defines wether or not the graph is played in standalone mode.
+     */
+    standalone: boolean;
+}
+
+export default class GraphEditorPlugin extends AbstractEditorPlugin<IGraphEditorPluginState> {
     /**
      * Defines the reference to the graph component.
      */
@@ -24,6 +43,11 @@ export default class GraphEditorPlugin extends AbstractEditorPlugin<{}> {
      */
     public constructor(props: IEditorPluginProps) {
         super(props);
+
+        this.state = {
+            playing: false,
+            standalone: false,
+        };
 
         this._components["graph"] = (
             <GraphComponent
@@ -40,8 +64,54 @@ export default class GraphEditorPlugin extends AbstractEditorPlugin<{}> {
     public render(): React.ReactNode {
         const layoutModel = Model.fromJson(layoutConfiguration);
 
+        const file = (
+            <Menu>
+                <MenuItem text="Load From..." icon={<Icon src="folder-open.svg" />} onClick={() => this._handleLoadFrom()} />
+                <MenuDivider />
+                <MenuItem text="Save (CTRL + S)" icon={<Icon src="copy.svg" />} onClick={() => this._handleSave()} />
+                <MenuItem text="Save As... (CTRL + SHIFT + S)" icon={<Icon src="copy.svg" />} onClick={() => this._handleSaveAs()} />
+            </Menu>
+        );
+
+        const edit = (
+            <Menu>
+                <MenuItem text="Undo (CTRL + Z)" icon={<Icon src="undo.svg" />} onClick={() => undoRedo.undo()} />
+                <MenuItem text="Redo (CTRL + Y)" icon={<Icon src="redo.svg" />} onClick={() => undoRedo.redo()} />
+                <MenuDivider />
+                <MenuItem text="Clear..." icon={<Icon src="recycle.svg" />} onClick={() => this._handleClearGraph()} />
+            </Menu>
+        );
+
         return (
-            <Layout model={layoutModel} factory={(n) => this._rootLayoutFactory(n)} />
+            <div style={{ width: "100%", height: "100%", overflow: "hidden" }}>
+                <div className="bp3-dark" style={{ width: "100%", height: "80px", backgroundColor: "#444444" }}>
+                    <ButtonGroup style={{ paddingTop: "4px" }}>
+                        <Popover content={file} position={Position.BOTTOM_LEFT}>
+                            <Button icon={<Icon src="folder-open.svg" />} rightIcon="caret-down" text="File" />
+                        </Popover>
+                        <Popover content={edit} position={Position.BOTTOM_LEFT}>
+                            <Button icon={<Icon src="edit.svg" />} rightIcon="caret-down" text="Edit" />
+                        </Popover>
+                    </ButtonGroup>
+                    <Divider />
+                    <ButtonGroup style={{ position: "relative", left: "50%", transform: "translate(-50%)" }}>
+                        <Button disabled={this.state.playing} icon={<Icon src="play.svg" />} rightIcon="caret-down" text="Play" onContextMenu={(e) => this._handlePlayContextMenu(e)} onClick={() => this._handleStart(false)} />
+                        <Button disabled={!this.state.playing} icon={<Icon src="square-full.svg" />} text="Stop" onClick={() => this._handleStop()} />
+                        <Button disabled={!this.state.playing} icon="reset" text="Restart" onClick={() => {
+                            this._handleStop();
+                            this._handleStart(this.state.standalone);
+                        }} />
+                    </ButtonGroup>
+                    <Divider />
+                </div>
+                <Layout model={layoutModel} factory={(n) => this._rootLayoutFactory(n)} classNameMapper={(c) => {
+                    if (c === "flexlayout__layout") {
+                        return "graphEditorFlexLayout";
+                    }
+
+                    return c;
+                }} />
+            </div>
         );
     }
 
@@ -57,6 +127,20 @@ export default class GraphEditorPlugin extends AbstractEditorPlugin<{}> {
      */
     public onClose(): void {
         // Empty for now...
+    }
+
+    /**
+     * Called on the plugin was previously hidden and is now shown.
+     */
+    public onShow(): void {
+        debugger;
+    }
+
+    /**
+     * Called on the plugin was previously visible and is now hidden.
+     */
+    public onHide(): void {
+        debugger;
     }
 
     /**
@@ -76,6 +160,34 @@ export default class GraphEditorPlugin extends AbstractEditorPlugin<{}> {
             <div>
                 {node.getName()}
             </div>
+        );
+    }
+
+    /**
+     * Called on the user wants to stop the graph.
+     */
+    private _handleStop(): void {
+        // TODO.
+    }
+
+    /**
+     * Called on the user wants to start the graph.
+     */
+    private _handleStart(standalone: boolean): void {
+        // TODO.
+        standalone;
+    }
+
+    /**
+     * Called on the user right-clicks on the "play" button.
+     */
+    private _handlePlayContextMenu(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+        ContextMenu.show(
+            <Menu className={Classes.DARK}>
+                <MenuItem text="Play" icon={<Icon src="play.svg" />} onClick={() => this._handleStart(false)} />
+                <MenuItem text="Play Only Current Graph" icon={<Icon src="play.svg" />} onClick={() => this._handleStart(true)} />
+            </Menu>,
+            { left: e.clientX, top: e.clientY },
         );
     }
 }
